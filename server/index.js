58e9b00752a0b480c0bdeb7f976a8c07a3e19cfa
DeveloperYamin <developer.yamin@gmail.com>
2022-12-23 17:14:10 +0600

    changes .env value

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use("/api/v1", router);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
