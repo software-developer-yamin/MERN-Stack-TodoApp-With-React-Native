139ec7596165e3abc3cbb8057da3aa78c937c855
DeveloperYamin <developer.yamin@gmail.com>
2022-12-23 16:35:52 +0600

    add serverUrl

--- mobile/redux/action.js
+++ mobile/redux/action.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverUrl = "";
+const serverUrl = "https://developeryamin-mern-stack-todoapp-with.onrender.com/api/v1";
 
 export const login = (email, password) => async (dispatch) => {
   try {
